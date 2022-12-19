3f97542c98dac8b975f2cabaa1b2b7d64a20aa63
th3w1ll <patrik.korab@gmail.com>
2022-12-19 10:35:24 +0100

    Initial Commit

---
+++ Besiktarn/BesiktarnApp.swift
@@ -0,0 +1,17 @@
+//
+//  BesiktarnApp.swift
+//  Besiktarn
+//
+//  Created by Patrik Korab on 2022-12-19.
+//
+
+import SwiftUI
+
+@main
+struct BesiktarnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
